6c8b75edd2115353253eb62236922b81d872e29b
Guilherme Lucca Casali <guilhermecasali.dev@gmail.com>
2021-12-24 20:01:23 -0300

    Added files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const gameButton = document.getElementById("button")
+const score = document.getElementById("score")
+let actualScore = 0
+
+let interval = 3500
+
+function shineRed() {
+    gameButton.classList.add("active")
+    setTimeout(() => {gameButton.classList.remove("active")}, 300)
+    console.log("Olá mundo!")
+    setTimeout(shineRed, (interval + Math.floor(Math.random() * (2000 + 2000) - 2000)))
+}
+
+function handlePoints() {
+    if (gameButton.classList.contains("active")) {
+        actualScore = actualScore + 3
+    } else {
+        actualScore = actualScore - 1
+    }
+    score.innerHTML = actualScore
+    console.log("Pontos mudaram", )
+}
+
+shineRed()
+
+gameButton.addEventListener("mousedown", () => {handlePoints()}, false)
